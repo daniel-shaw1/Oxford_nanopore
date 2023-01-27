7eb9ce05253ac83333e300aafcfdb4a1a18490c8
Daniel Shaw <43077619+daniel-shaw1@users.noreply.github.com>
2023-01-27 14:51:11 -0500

    Update canu_assembly.sh

--- canu_assembly.sh
+++ canu_assembly.sh
@@ -2,7 +2,7 @@
 #SBATCH --job-name=j_canu
 #SBATCH --partition=highmem_p
 #SBATCH --ntasks=1
-#SBATCH --mem=200gb
+#SBATCH --mem=256gb
 #SBATCH --time=168:00:00
 #SBATCH --output=canu.%j.out
 #SBATCH --error=canu.%j.err
@@ -12,10 +12,14 @@ cd /scratch/des65576/nanopore_seq_data/5130-2/gpu/
 
 ml canu/2.2-GCCcore-8.3.0-Java-11
 
-canu gridOptions=" --partition=batch --ntasks=20 --cpus-per-task=2 --time=168:00:00 " \
- -p stickleback -d /scratch/des65576/nanopore_seq_data/5130-2/gpu/assembly3/ \
+canu gridOptions=" --partition=highmem_p --ntasks=1 --cpus-per-task=6 --time=168:00:00 " \
+ -p stickleback -d /scratch/des65576/nanopore_seq_data/5130-2/gpu/assembly6/ \
  genomeSize=460m minInputCoverage=0 stopOnLowCoverage=0 \
+mhapPipe=false purgeOverlaps=false saveOverlaps=true \
  -nanopore-raw /scratch/des65576/nanopore_seq_data/5130-2/gpu/reads_full.fastq
+canu.sh (END)
+
+
 
 
 
